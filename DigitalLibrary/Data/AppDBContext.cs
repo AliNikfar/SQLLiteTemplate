e54b1a2e2709f0c4a3a1d7d26989ed1a273fb98f
alinikfarPDN <ali.nikfar2000@gmail.com>
2025-08-19 11:29:28 +0330

    Add Pakages , DBContexte ,connectionString , Book model , ..

---
+++ DigitalLibrary/Data/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using DigitalLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalLibrary.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options) 
+        {
+
+        }
+        public DbSet<Book> Books;
+    }
+}
